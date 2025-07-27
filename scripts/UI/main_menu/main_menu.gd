c773445dc384dbd941b661c8c51f4ccad92f418d
Goshva <42236284+SirNoods@users.noreply.github.com>
2025-07-27 20:16:08 +0200

    initial multiplayer testing

--- scripts/UI/main_menu/main_menu.gd
+++ scripts/UI/main_menu/main_menu.gd
@@ -1,14 +1,30 @@
 extends Control
 
+const PORT: int = 3000
+
+var main_scene: PackedScene = preload("uid://cb3u7k67b2t3f")
+
 @onready var host_button: Button = $HBoxContainer/HostButton
-@onready var play_button: Button = $HBoxContainer/PlayButton
+@onready var join_button: Button = $HBoxContainer/JoinButton
 
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_pressed)
-	play_button.pressed.connect(_on_play_pressed)
+	join_button.pressed.connect(_on_play_pressed)
+	multiplayer.peer_connected.connect(_on_peer_connected)
 	
 func _on_host_pressed() -> void:
-	print("host pressed")
+	var server_peer := ENetMultiplayerPeer.new()
+	server_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = server_peer
+	get_tree().change_scene_to_packed(main_scene)
+	
 	
 func _on_play_pressed() -> void:
-	print("play pressed")
+	var client_peer := ENetMultiplayerPeer.new()
+	client_peer.create_client("127.0.0.1", PORT)
+	multiplayer.multiplayer_peer = client_peer
+	get_tree().change_scene_to_packed(main_scene)
+	
+func _on_peer_connected(id: int):
+	print("my peer id: %s" % multiplayer.get_unique_id())
+	print("peer connected %s" % id)
